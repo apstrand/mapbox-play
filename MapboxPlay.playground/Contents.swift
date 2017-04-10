4288533d3a048b6ef61ebf4c90bdd1af040238c8
Peter Strand <peter@nena.se>
2017-04-10 01:33:07 -0400

    Show simple mapview in playground

--- MapboxPlay.playground/Contents.swift
+++ MapboxPlay.playground/Contents.swift
@@ -3,10 +3,20 @@ import UIKit
 
 import Wrapper
 import Mapbox
+import PlaygroundSupport
 
 let access_token = ""
 
 
 MGLAccountManager.setAccessToken(access_token)
 
+let pct_map_url = "mapbox://styles/apstrand/cj1as9wqo00482sljk3a14967"
 
+
+let mapView = MGLMapView(frame: CGRect(x:0, y:0, width:200, height:300), styleURL: URL(string: pct_map_url))
+
+mapView.setCenter(CLLocationCoordinate2D(latitude: 36.018540,
+                                         longitude: -118.123580),
+                  zoomLevel: 8, animated: false)
+
+PlaygroundPage.current.liveView = mapView
